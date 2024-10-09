a3a3a7e309c249cb80e91bba570401604051c373
sour-questionmark <soraya.ahmadi@student.tersterre.be>
2024-10-09 09:52:21 +0200

    JAVASCRIPT GRFRRRRRR

---
+++ HTMLok.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+/* console.log("WERKT GOE") */
+
+function showMore() {
+	document.getElementById("stuck").textContent="*3 minutes of free calling. 30 dollars per minute after the free trial has ended bla bla bla";
+	
+}
+
+function showLess() {
+	document.getElementById("stuck").textContent="FREE* dating advice from SOFANTH IEL";
+	
+}
+function onHover() {
+		document.getElementById("blink").src="foto/evil_louis.png";
+}
+
+function offHover() {
+	document.getElementById("blink").src="foto/DatingSim_blush.png";
+}
